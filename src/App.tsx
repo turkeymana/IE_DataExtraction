3b0c1a6b41da59a7af33163498c3db7e56ef9477
turkeymana <callum.morham@gmail.com>
2025-04-10 14:10:26 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FileUpload from './FileUpload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const handleFileUpload = (file: File) => {
+        console.log(file);
+    };
+
+    return (
+        <div>
+            <FileUpload onFileUpload={handleFileUpload} />
+        </div>
+    );
+};
 
 export default App;
